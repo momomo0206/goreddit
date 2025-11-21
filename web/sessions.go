30b1ba45b1ba0b28b358de462913b00d581a5f94
momomo0206 <momomo0206@gmail.com>
2025-11-21 19:38:56 +0900

    06: form validation

--- web/sessions.go
+++ web/sessions.go
@@ -21,6 +21,7 @@ func NewSessionManager(dataSourceName string) (*scs.SessionManager, error) {
 
 type SessionData struct {
 	FlashMessage string
+	Form         interface{}
 	// UserID uuid.UUID
 }
 
@@ -29,6 +30,11 @@ func GetSessionData(session *scs.SessionManager, ctx context.Context) SessionDat
 
 	data.FlashMessage = session.PopString(ctx, "flash")
 	// data.UserID, _ = session.Get(ctx, "user_id").(uuid.UUID)
+
+	data.Form = session.Pop(ctx, "form")
+	if data.Form == nil {
+		data.Form = map[string]string{}
+	}
 
 	return data
 }
